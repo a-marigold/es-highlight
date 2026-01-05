4c68cb3744d5afc5f61ee01bdc4adedeccb26f2f
NextKolya <phelixgluy@gmail.com>
2026-01-05 18:54:49 +0600

    add postcss with rollup-plugin-postcss configuration for css modules

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
+import postcss from 'rollup-plugin-postcss';
+
 import terser from '@rollup/plugin-terser';
 
 export default defineConfig([
@@ -15,13 +17,21 @@ export default defineConfig([
             file: './dist/index.js',
             format: 'esm',
         },
-        plugins: [typescript({ exclude: ['**__tests__/**'] }), terser()],
+        plugins: [
+            typescript({ exclude: ['**__tests__/**'] }),
+            terser(),
+            postcss({
+                minimize: true,
+                modules: { generateScopedName: '[local]__[hash:base64:6]' },
+                extract: true,
+            }),
+        ],
     },
 
     {
         input: './src/index.ts',
 
-        external: ['./src/__tests_/**'],
+        external: ['./src/__tests__/**'],
 
         output: {
             file: './dist/index.d.ts',
